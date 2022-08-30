6e5fe68cc9e0498641f910bd3fdec40fa01dafc2
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2022-08-30 13:02:33 -0600

    colorin colorado

--- SmithRebecca_Proyecto1.py
+++ SmithRebecca_Proyecto1.py
@@ -30,8 +30,8 @@ rend.active_shader = normalMap
 
 
 rend.glLoadModel("cookie.obj",
-                translate = V3(-1, -1, -10),
-                scale = V3(0.4,0.4,0.4),
+                translate = V3(-1, 1, -10),
+                scale = V3(0.6,0.6,0.6),
                 rotate = V3(0, -65, -90)
                 )
 
@@ -41,44 +41,46 @@ rend.active_shader = sabor
 
 # Oreo cookie
 rend.glLoadModel("oreo.obj",
-                translate = V3(1, 1, -10),
-                scale = V3(0.01,0.01,0.01),
+                translate = V3(1.5, 1, -10),
+                scale = V3(0.02,0.02,0.02),
                 rotate = V3(0, -70, 90)
                 )
 
-# rend.active_texture = Texture("pan.bmp")
-# # rend.active_texture2 = Texture("pannormal.bmp")
-# rend.active_shader = sabor
+rend.active_texture = Texture("pan.bmp")
+# rend.active_texture2 = Texture("pannormal.bmp")
+rend.active_shader = sabor
 
 
-# # Crossiant bread
-# rend.glLoadModel("bread.obj",
-#                 translate = V3(1, -1.9, -10),
-#                 scale = V3(0.5,0.5,0.5),
-#                 rotate = V3(90, 0, 0)
-#                 )
+# Crossiant bread
+rend.glLoadModel("bread.obj",
+                translate = V3(1, -1.6, -10),
+                scale = V3(0.4,0.4,0.4),
+                rotate = V3(90, 0, 0)
+                )
 
 
-# rend.active_texture = Texture("ceramic.bmp")
-# rend.active_texture2 = Texture("ceramicnormal.bmp")
-# rend.active_shader = gourad
-# # coffee cup
+rend.active_texture = Texture("ceramic.bmp")
+rend.active_texture2 = Texture("ceramicnormal.bmp")
+rend.active_shader = gourad
+# coffee cup
 
-# rend.glLoadModel("cupa.obj",
-#                 translate = V3(-1.1, 4, -10),
-#                 scale = V3(0.6,0.6,0.6),
-#                 rotate = V3(0, -90, -90)
-#                 )
-# # Dona
-# rend.active_texture = Texture("banana.bmp")
-# # rend.active_texture2 = Texture("donasombra.bmp")
-# rend.active_shader = flat
+rend.glLoadModel("cupa.obj",
+                translate = V3(-1.1, 4, -10),
+                scale = V3(0.6,0.6,0.6),
+                rotate = V3(0, -90, -90)
+                )
 
-# rend.glLoadModel("banana.obj",
-#                 translate = V3(4, 1, -10),
-#                 scale = V3(20,20,20),
-#                 rotate = V3(0, -180, -90)
-#                 )
+
+# Banana
+rend.active_texture = Texture("banana.bmp")
+# rend.active_texture2 = Texture("donasombra.bmp")
+rend.active_shader = flat
+
+rend.glLoadModel("banana.obj",
+                translate = V3(6, 1, -10),
+                scale = V3(20,20,20),
+                rotate = V3(0, -180, -90)
+                )
 
 rend.glFinish("output.bmp")
 
